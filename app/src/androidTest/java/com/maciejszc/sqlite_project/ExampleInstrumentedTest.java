823d90d80ddb205fd7b074a69a3234138a6b15f5
Maciej <maciej.szczesny.orzesze@gmail.com>
2019-07-05 19:34:36 +0200

    update 0.1

--- app/src/androidTest/java/com/maciejszc/sqlite_project/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/maciejszc/sqlite_project/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.hellohasan.sqlite_project;
+package com.maciejszc.sqlite_project;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -9,11 +9,7 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
@@ -21,6 +17,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.hellohasan.sqlite_project", appContext.getPackageName());
+        assertEquals("com.maciejszc.sqlite_project", appContext.getPackageName());
     }
 }
